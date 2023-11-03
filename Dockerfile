93723b6d01ab1c06361216464c81a68de64d0c3c
ikigai0822 <pranaymayal17@gmail.com>
2023-11-03 18:24:31 +0530

    added java app &dockerfile

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM openjdk:11.0
+COPY target\calc-0.0.1.jar calc-0.0.1.jar
---
+++ src/main/java/devopsb2cicd/lab3/Myclass.java
@@ -0,0 +1,19 @@
+package devopsb2cicd.lab3;
+
+/**
+ * Hello world!
+ *
+ */
+public class Myclass {
+    public void greetings() {
+        System.out.println("Hello Github Actions");
+    }
+
+    public void testMsg() {
+        System.out.println("This is the test msg");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
